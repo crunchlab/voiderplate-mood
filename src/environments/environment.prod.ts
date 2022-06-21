c129e73300ae08172710bd321d50a3fec7d05d7e
Lorenzo Brutti <lbrutti@users.noreply.github.com>
2022-06-21 11:52:46 +0200

    recupere versione da FullSemVer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 import featureToStrutturaMappings from "src/mappings/featureToStrutturaMappings";
 import filtersFieldMappings from "../mappings/filtersFieldMappings";
+import { FullSemVer } from "../assets/version.json";
 
 export const environment = {
     production: true,
@@ -7,6 +8,6 @@ export const environment = {
     filtersFieldMappings: filtersFieldMappings,
     mapStyle: './assets/map-styles/roadmap-style.json',
     dataPointColorMap: './assets/map-styles/data-points-colors.json',
-    version: "1.0.0-beta"
+    version: FullSemVer
 
 };
